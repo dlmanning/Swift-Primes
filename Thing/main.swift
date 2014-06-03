306d40d9fe3d54e52f499df21f08cdc7987fe6bd
David Manning <dlmanning@me.com>
2014-06-02 23:05:17 -0700

    Initial Commit

---
+++ Thing/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Thing
+//
+//  Created by David Manning on 6/2/14.
+//  Copyright (c) 2014 David Manning. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
